7d37f173f7161d7a8f2853690d57588217861bb8
Jose Sorli <jose.sorli.esteve@hotmail.com>
2024-03-11 19:38:52 +0100

    version inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Konan Constraint Layout"
+include(":app")
+ 
